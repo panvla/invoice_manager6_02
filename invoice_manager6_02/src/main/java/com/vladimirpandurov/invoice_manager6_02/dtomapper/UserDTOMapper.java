c010b4e687d6c5772b25cacdfa07659418696a33
panvla <pandvlado@gmail.com>
2024-02-16 18:40:58 +0100

    user dot defined.

---
+++ invoice_manager6_02/src/main/java/com/vladimirpandurov/invoice_manager6_02/dtomapper/UserDTOMapper.java
@@ -0,0 +1,23 @@
+package com.vladimirpandurov.invoice_manager6_02.dtomapper;
+
+import com.vladimirpandurov.invoice_manager6_02.domain.User;
+import com.vladimirpandurov.invoice_manager6_02.dto.UserDTO;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDTOMapper {
+
+    public static UserDTO fromUser(User user){
+        UserDTO userDTO = new UserDTO();
+        BeanUtils.copyProperties(user, userDTO);
+        return userDTO;
+    }
+
+    public static User toUser(UserDTO userDTO){
+        User user = new User();
+        BeanUtils.copyProperties(userDTO, user);
+        return user;
+    }
+
+}
